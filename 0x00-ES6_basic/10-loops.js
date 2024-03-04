bacf75ccfcb9839e9e270641cca7585eda31b022
nickfff-dev <hunchographics@gmail.com>
2024-03-04 21:21:24 +0000

    loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,4 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const newArray = array.map((value) => appendString + value);
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
+  }
+
   return newArray;
 }
