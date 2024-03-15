803c426502cff14bdb4fa6ee2fdac354e1a68749
nickfff-dev <hunchographics@gmail.com>
2024-03-15 22:25:23 +0000

    all

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,9 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([pRes, uRes]) => (console.log(pRes.body, uRes.firstName, uRes.lastName)))
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
