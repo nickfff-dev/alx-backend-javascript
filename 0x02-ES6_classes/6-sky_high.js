dcfaae65ca5289c71e59604010536002962d102e
nickfff-dev <hunchographics@gmail.com>
2024-03-21 18:27:14 +0000

    setter

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -14,6 +14,13 @@ export default class SkyHighBuilding extends Building {
     return this._floors; // Return the floors attribute
   }
 
+  set floors(floors) {
+    if (typeof floors !== 'number') {
+      throw new TypeError('Floors must be a number');
+    }
+    this._floors = floors;
+  }
+
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this.floors} floors.`; // Override the method to return a specific string
   }
