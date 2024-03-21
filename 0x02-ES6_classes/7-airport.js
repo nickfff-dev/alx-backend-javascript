90f87fa797653508802c61328150798b55390436
nickfff-dev <hunchographics@gmail.com>
2024-03-21 19:07:26 +0000

    airport

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,24 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  toString() {
+    return `Airport [${this.code}] { _name: '${this.name}', _code: '${this.code}' }`;
+  }
+}
