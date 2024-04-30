dacdf94bb6943944c3c1020d2941bf5551d1c95d
nickfff-dev <hunchographics@gmail.com>
2024-05-01 01:04:50 +0300

    console

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Prints a message to STDOUT.
+ * @param {string} message - The message to be printed.
+ */
+function displayMessage (message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
