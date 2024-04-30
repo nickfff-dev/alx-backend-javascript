1d94d6d3468025d0220f79ef8cee852ffc65070c
nickfff-dev <hunchographics@gmail.com>
2024-05-01 01:26:09 +0300

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,13 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdout.on('line', (input) => {
+  if (input) {
+    process.stdout.write(`Your name is: ${input}`);
+    process.exit(0);
+  }
 });
 
-console.log('Welcome to Holberton School, what is your name?');
-
-rl.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
-  rl.close();
-});
-
-rl.on('close', () => {
-  console.log('This important software is now closing');
+process.stdin.on('close', () => {
+  process.stdout.write('This important software is now closing');
   process.exit(0);
 });
