011353adf3643c3e33f4176ed9533c815a3cf0c5
nickfff-dev <hunchographics@gmail.com>
2024-05-01 01:31:00 +0300

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdout.write('Welcome to Holberton School, what is your name?');
 
-process.stdout.on('line', () => {
+process.stdout.on('readable', () => {
   const input = process.stdin.read();
   if (input) {
     process.stdout.write(`Your name is: ${input}`);
