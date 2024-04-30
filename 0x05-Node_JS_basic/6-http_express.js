07eab63e6f29f6964f0b00b00a732dc32d231c9c
nickfff-dev <hunchographics@gmail.com>
2024-05-01 02:36:09 +0300

    fixed

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -4,11 +4,11 @@ const app = express();
 const PORT = 1245;
 
 app.get('/', (_, res) => {
-  res.send('Hello Holberton School!\n');
+  res.send('Hello Holberton School!');
 });
 
 app.listen(1245, () => {
-  console.log(`Server Listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
 
 module.exports = app;
