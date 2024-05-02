31fd42e15c981e7684ca483e7a10cb2f7b549d8c
nickfff-dev <hunchographics@gmail.com>
2024-05-03 00:30:58 +0300

    one test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
+const { describe, it } = require('mocha');
 
 describe('calculateNumber', function() {
  it('should round and sum two numbers', function() {
@@ -8,15 +9,12 @@ describe('calculateNumber', function() {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
  });
- 
- describe('calculateNumber', function() {
-   it('should round to the nearest int', function() {
+   it('should round and sum two numbers', function() {
       assert.strictEqual(calculateNumber(1.4, 3.7), 5);
       assert.strictEqual(calculateNumber(1.5, 3.7), 6);
       assert.strictEqual(calculateNumber(1.6, 3.7), 6);
-      assert.strictEqual(calculateNumber(1.7, 3.7), 6);
+      assert.strictEqual(calculateNumber(-1.7, -1.6), -4);
    });
-  });
 });
 
 
