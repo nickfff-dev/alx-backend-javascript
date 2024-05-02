f06030da93b1637acf1f22e4a9d8bf3160e6cd4e
nickfff-dev <hunchographics@gmail.com>
2024-05-03 00:27:33 +0300

    one test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -8,13 +8,15 @@ describe('calculateNumber', function() {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
  });
+ 
+ describe('calculateNumber', function() {
+   it('should round to the nearest int', function() {
+      assert.strictEqual(calculateNumber(1.4, 3.7), 5);
+      assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+      assert.strictEqual(calculateNumber(1.6, 3.7), 6);
+      assert.strictEqual(calculateNumber(1.7, 3.7), 6);
+   });
+  });
 });
 
-describe('calculateNumber', function() {
- it('should round to the nearest int', function() {
-    assert.strictEqual(calculateNumber(1.4, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-    assert.strictEqual(calculateNumber(1.6, 3.7), 6);
-    assert.strictEqual(calculateNumber(1.7, 3.7), 6);
- });
-});
+
