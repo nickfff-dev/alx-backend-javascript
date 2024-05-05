aa22ee08274367668c58d52d9000a0377eb1cb96
nickfff-dev <hunchographics@gmail.com>
2024-05-05 20:56:14 +0300

    controllers

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -3,7 +3,8 @@ const { readDatabase } = require('../utils');
 class StudentsController {
   static async getAllStudents(req, res) {
     try {
-      const data = await readDatabase('./database.csv');
+      const path = process.argv.length > 2 ? process.argv[2] : '';
+      const data = await readDatabase(path);
       const fullResponse = [];
       fullResponse.push('This is the list of our students');
       Object.keys(data).sort().forEach((field) => {
@@ -16,12 +17,13 @@ class StudentsController {
   }
 
   static async getAllStudentsByMajor(req, res) {
+    const path = process.argv.length > 2 ? process.argv[2] : '';
     const { major } = req.params;
     if (major !== 'CS' && major !== 'SWE') {
       return res.status(500).send('Major parameter must be CS or SWE');
     }
     try {
-      const data = await readDatabase('./database.csv');
+      const data = await readDatabase(path);
       if (data[major]) {
         return res.status(200).send(`List: ${data[major].join(', ')}`);
       }
